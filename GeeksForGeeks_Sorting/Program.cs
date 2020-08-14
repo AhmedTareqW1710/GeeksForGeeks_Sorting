b0dd11519487f8255d41101daf6ffc4fee8a69fe
aebrahim <aebrahim@THQ1LTAEBRAHIM>
2020-08-14 20:15:21 +0300

    Adding Merge Sort for the solution

--- GeeksForGeeks_Sorting/Program.cs
+++ GeeksForGeeks_Sorting/Program.cs
@@ -42,6 +42,82 @@ namespace GeeksForGeeks_Sorting
         }
 
 
+        private static int[]  MergeSort(int[] a, int leftIndex, int rightIndex)
+        {
+
+            if (leftIndex < rightIndex)
+            {
+                int middleIndex = (leftIndex + rightIndex) / 2;
+
+                MergeSort(a, leftIndex, middleIndex);
+
+                MergeSort(a, middleIndex + 1, rightIndex);
+
+
+                Merge(a, leftIndex, middleIndex, rightIndex);
+
+            }
+
+            return a;
+        }
+
+        private static int[] Merge(int[] arr, int left, int middle, int right)
+        {
+            // Get length of the two new subarrays
+            int n1 = (middle - left) + 1;
+            int n2 = right - middle;
+
+            // Create two new arrays with the length calculated
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+
+            // Copy data to the new arrays 
+            for (int i = 0; i < n1; ++i)
+                L[i] = arr[left + i];
+            for (int j = 0; j < n2; ++j)
+                R[j] = arr[middle + 1 + j];
+
+            // merge the two temp arrays
+
+            // Initial indexes of first and second subarrays 
+            int m = 0;
+            int n = 0;
+
+            // Initial index of merged subarry array 
+            int k = left;
+            while (m < n1 && n < n2)
+            {
+                if (L[m] <= R[n])
+                {
+                    arr[k] = L[m];
+                    m++;
+                }
+                else
+                {
+                    arr[k] = R[n];
+                    n++;
+                }
+                k++;
+            }
+
+            /* Copy remaining elements of L[] if any */
+            while (m < n1)
+            {
+                arr[k] = L[m];
+                m++;
+                k++;
+            }
+
+            /* Copy remaining elements of R[] if any */
+            while (n < n2)
+            {
+                arr[k] = R[n];
+                n++;
+                k++;
+            }
+
+            return arr;
+        }
 
         static void Main(string[] args)
         {
@@ -63,7 +139,7 @@ namespace GeeksForGeeks_Sorting
                     A[j] = Convert.ToInt32(arrStr[j].ToString());
                 }
 
-                int[] sortedArr = SelectionSort(A, n);
+                int[] sortedArr = MergeSort(A, 0,n -1);
 
                 string output = string.Empty;
 
